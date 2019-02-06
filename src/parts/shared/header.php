cba1b2f171e00d6260cb71e41a1356116d7ea4bf
mattwills8 <matt_wills8@hotmail.co.uk>
2019-02-05 22:51:47 -0600

    add header layout and styling

---
+++ src/parts/shared/header.php
@@ -0,0 +1,7 @@
+<?php
+
+$tq_header_style_1_color = '123'; // set it to something invalid to allow us to use the css
+
+include( locate_template( '/parts/shared/header-templates/header-style-1.php' , false, false ) );
+
+?>
