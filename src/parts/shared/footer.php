dae513d3a217c606d5368ec3d46308bd7ccd51c4
mattwills8 <matt_wills8@hotmail.co.uk>
2019-02-06 16:51:36 -0600

    add bottom section to footer

--- src/parts/shared/footer.php
+++ src/parts/shared/footer.php
@@ -1,56 +1,61 @@
 <footer>
-  <div class="footer-contacts-wrapper" >
-    <?php
 
-    $query = new WP_Query( array(
-        'post_type'       => 'torque_staff',
-        'posts_per_page'  => -1
-    ));
-
-    if ( $query->have_posts() ) {
-
-      ?>
-
-      <h2>CONTACT</h2>
-
-      <div class="footer-contacts">
-
+  <div class="footer-main" >
+    <div class="footer-contacts-wrapper" >
       <?php
 
-      while ( $query->have_posts() ) {
-        $query->the_post();
+      $query = new WP_Query( array(
+          'post_type'       => 'torque_staff',
+          'posts_per_page'  => -1
+      ));
 
-        $meta = get_post_meta( get_the_ID(), 'staff_meta', true );
+      if ( $query->have_posts() ) {
 
         ?>
 
-        <div class="footer-contact" >
-          <p class="footer-contact-field name" ><?php the_title(); ?></p>
+        <h2>CONTACT</h2>
 
-          <?php if ($meta['tel']) { ?>
-            <p class="footer-contact-field tel" ><?php echo $meta['tel']; ?></p>
-          <?php }?>
+        <div class="footer-contacts">
 
-          <?php if ($meta['email']) { ?>
-            <a href="mailto:<?php echo $meta['email']; ?>" >
-              <p class="footer-contact-field email" ><?php echo $meta['email']; ?></p>
-            </a>
-          <?php }?>
+        <?php
+
+        while ( $query->have_posts() ) {
+          $query->the_post();
+
+          $meta = get_post_meta( get_the_ID(), 'staff_meta', true );
+
+          ?>
+
+          <div class="footer-contact" >
+            <p class="footer-contact-field name" ><?php the_title(); ?></p>
+
+            <?php if ($meta['tel']) { ?>
+              <p class="footer-contact-field tel" ><?php echo $meta['tel']; ?></p>
+            <?php }?>
+
+            <?php if ($meta['email']) { ?>
+              <a href="mailto:<?php echo $meta['email']; ?>" >
+                <p class="footer-contact-field email" ><?php echo $meta['email']; ?></p>
+              </a>
+            <?php }?>
+          </div>
+
+          <?php
+        }
+
+        ?>
+
         </div>
 
         <?php
       }
 
+
+      wp_reset_postdata();
+
       ?>
-
-      </div>
-
-      <?php
-    }
-
-
-    wp_reset_postdata();
-
-    ?>
+    </div>
   </div>
+
+  <div class="footer-lower" ></div>
 </footer>
