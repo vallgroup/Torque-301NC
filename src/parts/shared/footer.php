3d2be2640fc8b03436ad612cfb7c61bafcf2a981
mattwills8 <matt_wills8@hotmail.co.uk>
2019-02-06 16:10:50 -0600

    add footer contacts and styling

---
+++ src/parts/shared/footer.php
@@ -0,0 +1,56 @@
+<footer>
+  <div class="footer-contacts-wrapper" >
+    <?php
+
+    $query = new WP_Query( array(
+        'post_type'       => 'torque_staff',
+        'posts_per_page'  => -1
+    ));
+
+    if ( $query->have_posts() ) {
+
+      ?>
+
+      <h2>CONTACT</h2>
+
+      <div class="footer-contacts">
+
+      <?php
+
+      while ( $query->have_posts() ) {
+        $query->the_post();
+
+        $meta = get_post_meta( get_the_ID(), 'staff_meta', true );
+
+        ?>
+
+        <div class="footer-contact" >
+          <p class="footer-contact-field name" ><?php the_title(); ?></p>
+
+          <?php if ($meta['tel']) { ?>
+            <p class="footer-contact-field tel" ><?php echo $meta['tel']; ?></p>
+          <?php }?>
+
+          <?php if ($meta['email']) { ?>
+            <a href="mailto:<?php echo $meta['email']; ?>" >
+              <p class="footer-contact-field email" ><?php echo $meta['email']; ?></p>
+            </a>
+          <?php }?>
+        </div>
+
+        <?php
+      }
+
+      ?>
+
+      </div>
+
+      <?php
+    }
+
+
+    wp_reset_postdata();
+
+    ?>
+  </div>
+</footer>
