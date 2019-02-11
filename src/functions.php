f359fed407126dc3d5fda9c55d4ab6ab89dc94a2
mattwills8 <matt_wills8@hotmail.co.uk>
2019-02-11 15:02:27 -0600

    rename to 318

--- src/functions.php
+++ src/functions.php
@@ -59,10 +59,10 @@ if ( class_exists( 'Torque_Map_Controller' ) ) {
  function torque_remove_menus(){
 
    //remove_menu_page( 'index.php' );                  //Dashboard
-   //remove_menu_page( 'edit.php' );                   //Posts
+   remove_menu_page( 'edit.php' );                   //Posts
    //remove_menu_page( 'upload.php' );                 //Media
    //remove_menu_page( 'edit.php?post_type=page' );    //Pages
-   //remove_menu_page( 'edit-comments.php' );          //Comments
+   remove_menu_page( 'edit-comments.php' );          //Comments
    //remove_menu_page( 'themes.php' );                 //Appearance
    //remove_menu_page( 'plugins.php' );                //Plugins
    //remove_menu_page( 'users.php' );                  //Users
@@ -92,7 +92,7 @@ function torque_enqueue_child_styles() {
     wp_enqueue_style( $parent_main_style, get_template_directory_uri() . '/bundles/main.css' );
 
     // enqueue child style
-    wp_enqueue_style( '301nc-styles',
+    wp_enqueue_style( '318nc-styles',
         get_stylesheet_directory_uri() . '/bundles/main.css',
         array( $parent_style, $parent_main_style ),
         wp_get_theme()->get('Version')
@@ -103,7 +103,7 @@ function torque_enqueue_child_styles() {
 add_action( 'wp_enqueue_scripts', 'torque_enqueue_child_scripts');
 function torque_enqueue_child_scripts() {
 
-    wp_enqueue_script( '301nc-script',
+    wp_enqueue_script( '318nc-script',
         get_stylesheet_directory_uri() . '/bundles/bundle.js',
         array( 'torque-theme-scripts' ), // depends on parent script
         wp_get_theme()->get('Version'),
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 const srcDir = path.join(__dirname, 'src')
-const buildDir = path.join(projectConfig.root, 'wp-content/themes/301nc')
+const buildDir = path.join(projectConfig.root, 'wp-content/themes/318nc')
 
 const config = {
   context: srcDir,
